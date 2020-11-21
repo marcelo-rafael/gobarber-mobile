4ef562fc053e89a021f08a02df08f5773bac5511
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2020-11-21 18:47:10 -0300

    autenticação de usuários

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import {
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './src/hooks';
+
 import Routes from './src/routes';
 
 const App: React.FC = () => {
@@ -24,9 +26,11 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   );
 };
